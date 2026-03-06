a3f54e9f334b194c05e55f92ac3ce53bd8cf8169
Devious-Unicorn <gslawrence27@gmail.com>
2026-03-06 15:16:41 -0700

    create minimap border and adjust minimap boat size

--- assets/scripts/sub_viewport.gd
+++ assets/scripts/sub_viewport.gd
@@ -7,7 +7,7 @@ func _ready():
 	world_2d = get_tree().root.get_viewport().world_2d
 	get_tree().root.canvas_cull_mask = 1
 	$"Minimap camera".zoom = Vector2.ONE / 15
-	MinimapIcon.scale = Vector2.ONE * 4
+	MinimapIcon.scale = Vector2.ONE * 5
 
 func _process(delta: float) -> void:
 	$"Minimap camera".global_position = Boat.global_position
