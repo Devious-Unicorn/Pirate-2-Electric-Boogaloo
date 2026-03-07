b082f0acd68375a4315d7aee01a8f829543a050e
Devious-Unicorn <gslawrence27@gmail.com>
2026-03-06 20:40:23 -0700

    add input remapping

--- assets/scripts/options_menu.gd
+++ assets/scripts/options_menu.gd
@@ -1,1 +1,30 @@
 extends Control
+
+var ctrls: Array = []
+var listening := false
+var listener = null
+
+func _unhandled_input(event: InputEvent) -> void:
+	if not listening: return
+	InputMap.action_add_event(listener.get_parent().name, event)
+	updateLabels()
+
+func _ready() -> void:
+	$Quit.pressed.connect(queue_free)
+	for node in find_children("*", "Button"):
+		if(node is Button and node.name == "ctrl"):
+			ctrls.append(node)
+			node.pressed.connect(setCtrls.bind(node))
+	
+	updateLabels()
+
+func updateLabels():
+	for ctrl in ctrls:
+		InputMap.action_get_events(ctrl.get_parent().name)
+		ctrl.text = InputMap.action_get_events(ctrl.get_parent().name)[0].as_text().replace(" - Physical", "")
+
+func setCtrls(button: Button):
+	button.text = "Awaiting input..."
+	listening = true;
+	listener = button
+	InputMap.action_erase_events(button.get_parent().name)
--- assets/scripts/play_menu.gd
+++ assets/scripts/play_menu.gd
@@ -1,8 +1,11 @@
 extends Control
 
 @onready var newGame = $"MarginContainer/VBoxContainer/New game"
+@onready var quit = $MarginContainer/VBoxContainer/Quit
 @onready var Main = get_parent()
 
 func _ready() -> void:
 	newGame.pressed.connect(Callable(Main, "newGame"))
 	newGame.pressed.connect(queue_free)
+	quit.pressed.connect(Callable(Main, "_ready"))
+	quit.pressed.connect(queue_free)
